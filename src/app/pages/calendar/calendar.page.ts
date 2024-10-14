ae60d8d5b4fd8bff16a3c3635a6be5309f06a12a
BetoSoTe <rsototellez@hotmail.com>
2024-10-14 13:14:04 -0600

    Se agrega los segmentos en el calendario

--- src/app/pages/calendar/calendar.page.ts
+++ src/app/pages/calendar/calendar.page.ts
@@ -1,32 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonDatetime, IonIcon } from '@ionic/angular/standalone';
-import { NgCalendarModule, CalendarMode } from 'ionic2-calendar';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonDatetime, IonIcon, IonSegment, IonSegmentButton, IonLabel } from '@ionic/angular/standalone';
+//import { NgCalendarModule, CalendarMode } from 'ionic2-calendar';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.page.html',
   styleUrls: ['./calendar.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonDatetime, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NgCalendarModule]
+  imports: [IonLabel, IonSegmentButton, IonSegment, IonIcon, IonDatetime, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, /*NgCalendarModule*/]
 })
 export class CalendarPage implements OnInit {
-  eventSource = []; // Aquí puedes cargar eventos desde tu servicio o agregar eventos manualmente
-  calendarMode: CalendarMode = 'month'; // Puedes cambiar entre 'month', 'week' y 'day'
-  currentDate: Date = new Date();
+  selectedSegment: string = 'mes';
 
   constructor() { }
 
   ngOnInit() {}
 
-  onEventSelected(event: any) { // Usa `any` si no estás seguro del tipo
-    console.log('Evento seleccionado:', event);
-    // Aquí puedes mostrar detalles del evento seleccionado
-  }
-
-  onDateChanged(event: any) {
-    this.currentDate = new Date(event.detail.value); // Cambia la fecha actual
-    console.log('Fecha seleccionada:', this.currentDate);
+  segmentChanged(event: any) {
+    this.selectedSegment = event.detail.value;
   }
 }
